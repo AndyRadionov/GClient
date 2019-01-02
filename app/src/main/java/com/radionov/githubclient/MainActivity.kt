85588c2b91cea3fcde48eafeae195efe3c8cc221
Andrey Radionov <andyomsk@gmail.com>
2019-01-02 23:47:55 +0600

    Authorization

--- app/src/main/java/com/radionov/githubclient/MainActivity.kt
+++ app/src/main/java/com/radionov/githubclient/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.radionov.githubclient
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onAuthClick(v: View) {
+        startActivity(Intent(this, OAuthActivity::class.java))
     }
 }
